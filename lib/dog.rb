5f3e8cf32d0606cce84892bbdde921035fa36a1a
Rob Parker <into.the.parker@gmail.com>
2018-10-10 08:53:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ class Dog
   def initialize(name)
     @name = name
   end  
-  def breed=(breed)
-    @breed = breed
-  end
     def initialize(breed)
     @breed = breed
   end  
+   def breed=(breed)
+    @breed = breed
+  end
 end
